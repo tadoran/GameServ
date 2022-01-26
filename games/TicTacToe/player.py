90ef58a29b3215bb05b9394039f9681e45688c48
Gorelov Konstantin <gorelovkg@yandex.ru>
2022-01-27 02:12:57 +0300

    TicTacToe refactoring

---
+++ games/TicTacToe/player.py
@@ -0,0 +1,74 @@
+from abc import ABC, abstractmethod
+from random import choice
+from time import sleep
+
+import numpy as np
+
+from games import spectator
+
+
+class AbstractPlayer(ABC):
+    def __init__(self, game):
+        self.game = game
+
+
+
+class AbstractServerSideProxy:
+    pass
+
+
+class AbstractClientSideProxy:
+    pass
+
+
+class TicTacToeAbstractPlayer(AbstractPlayer):
+    def __init__(self, game=None):
+        super().__init__(game)
+
+    def make_move(self, coords):
+        if not self.game:
+            raise AttributeError("Game is not initialized")
+        else:
+            return self.game.make_move(self, coords)
+
+    @abstractmethod
+    def get_turn(self):
+        pass
+
+    @property
+    def position(self):
+        return self.game.get_player_position(self)
+
+    def __str__(self):
+        return f"Player {self.position}"
+
+
+class TicTacToePersonPlayer(TicTacToeAbstractPlayer):
+    def get_turn(self):
+        valid = False
+        x, y = 0, 0
+
+        while not valid:
+            coords_to_hit_str = input("Select x,y to place your mark:\n")
+            try:
+                x, y = map(int, coords_to_hit_str.split(","))
+                valid = True
+            except ValueError:
+                spectator.print("Invalid input")
+                valid = False
+
+        return self.make_move((x - 1, y - 1))
+
+    def __str__(self):
+        return "Kostia"
+
+
+class TicTacToeComputerPlayer(TicTacToeAbstractPlayer):
+    def get_turn(self):
+        empty_cells_raw = np.where(self.game.field == 0)
+        cell_to_fill = choice(tuple(zip(empty_cells_raw[0], empty_cells_raw[1])))
+        sleep(0.8)
+        return self.make_move(cell_to_fill)
+
+    def __str__(self):
+        return "Computer"
